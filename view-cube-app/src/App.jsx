e9f69d637b593b7f011ad43ee69db42724f07979
VizArcher <viztech2002@gmail.com>
2026-01-26 20:12:39 +0530

    Fixed the UI of the application through App.jsx

--- view-cube-app/src/App.jsx
+++ view-cube-app/src/App.jsx
@@ -1,81 +1,81 @@
-import React from 'react';
-import TrameViewer from './components/MainViewer/TrameViewer';
-import ViewCube from './components/ViewCube/ViewCube';
-import { Page, PageSection } from '@patternfly/react-core';
-import '@patternfly/react-core/dist/styles/base.css';
-
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return (
-            <Page className="full-height">
-                <PageSection
-                    className="full-height"
-                    style={{ padding: '0px', width: '50%' }}
-                >
-                    {/* The Main Viewer (Background) */}
-                    <TrameViewer className="full-height" url="http://localhost:8080/" viewerId="viewer1" />
-                </PageSection>
-                <PageSection
-                    className="full-height"
-                    style={{ padding: '0px', width: '50%'}}
-                >
-                    {/* The UI Overplay (Foreground) */}
-                    <ViewCube />
-
-                    {/* Optional: Simple instruction overlay */}
-                    <div style={{
-                        position: 'absolute',
-                        bottom: '20px',
-                        left: '20px',
-                        color: 'white',
-                        fontFamily: 'sans-serif',
-                        pointerEvents: 'none',
-                        opacity: 0.7
-                    }}>
-                        <p>• Drag background to orbit</p>
-                        <p>• Click Cube faces to animate</p>
-                    </div>
-                </PageSection>
-            </Page>
-        );
-    }
-}
-
-// import ThreeScene from './components/MainViewer/ThreeScene';
+// import React from 'react';
 // import TrameViewer from './components/MainViewer/TrameViewer';
 // import ViewCube from './components/ViewCube/ViewCube';
+// import { Page, PageSection } from '@patternfly/react-core';
+// import '@patternfly/react-core/dist/styles/base.css';
 // 
-// function App() {
-//     return (
-//         <div>
-//             {/* The Main Viewer (Background) */}
-//             <TrameViewer className="full-height" url="http://localhost:8080/" viewerId="viewer1" />
+// export default class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//     
+//     render() {
+//         return (
+//             <Page className="full-height">
+//                 <PageSection
+//                     className="full-height"
+//                     style={{ padding: '0px', width: '50%' }}
+//                 >
+//                     {/* The Main Viewer (Background) */}
+//                     <TrameViewer className="full-height" url="http://localhost:8080/" viewerId="viewer1" />
+//                 </PageSection>
+//                 <PageSection
+//                     className="full-height"
+//                     style={{ padding: '0px', width: '50%'}}
+//                 >
+//                     {/* The UI Overplay (Foreground) */}
+//                     <ViewCube />
 // 
-//             {/* The UI Overplay (Foreground) */}
-//             <ViewCube />
-// 
-//             {/* Optional: Simple instruction overlay */}
-//             <div style={{
-//                 position: 'absolute',
-//                 bottom: '20px',
-//                 left: '20px',
-//                 color: 'white',
-//                 fontFamily: 'sans-serif',
-//                 pointerEvents: 'none',
-//                 opacity: 0.7
-//             }}>
-//                 <p>• Drag background to orbit</p>
-//                 <p>• Click Cube faces to animate</p>
-//             </div>
-//         </div>
-//     );
+//                     {/* Optional: Simple instruction overlay */}
+//                     <div style={{
+//                         position: 'absolute',
+//                         bottom: '20px',
+//                         left: '20px',
+//                         color: 'white',
+//                         fontFamily: 'sans-serif',
+//                         pointerEvents: 'none',
+//                         opacity: 0.7
+//                     }}>
+//                         <p>• Drag background to orbit</p>
+//                         <p>• Click Cube faces to animate</p>
+//                     </div>
+//                 </PageSection>
+//             </Page>
+//         );
+//     }
 // }
-// 
-// export default App; 
+
+import ThreeScene from './components/MainViewer/ThreeScene';
+import TrameViewer from './components/MainViewer/TrameViewer';
+import ViewCube from './components/ViewCube/ViewCube';
+
+function App() {
+    return (
+        <div>
+            {/* The Main Viewer (Background) */}
+            <TrameViewer className="full-height" url="http://localhost:8080/" viewerId="viewer1" />
+
+            {/* The UI Overplay (Foreground) */}
+            <ViewCube />
+
+            {/* Optional: Simple instruction overlay */}
+            <div style={{
+                position: 'absolute',
+                bottom: '20px',
+                left: '20px',
+                color: 'white',
+                fontFamily: 'sans-serif',
+                pointerEvents: 'none',
+                opacity: 0.7
+            }}>
+                <p>• Drag background to orbit</p>
+                <p>• Click Cube faces to animate</p>
+            </div>
+        </div>
+    );
+}
+
+export default App
 
 // import TrameViewer from './components/MainViewer/TrameViewer';
 // import ViewCube from './components/ViewCube/ViewCube';
